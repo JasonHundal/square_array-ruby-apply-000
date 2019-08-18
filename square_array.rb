134f64dde5a2c0a690b6961b636185317df37ede
Jason Hundal <jasonhundal23@gmail.com>
2019-08-18 21:01:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
 while array [counter] do
-    new_array << array[counter] **2 
+    new_array << array[counter] ** 2 
     counter += 1 
     new_array
 end
